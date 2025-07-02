3a77b52f9126b0047a4b1075c109e7f40bfe13c1
vaishnavi <vaishnavishaw335@gmail.com>
2025-07-02 21:10:37 +0530

    products page

--- src/pages/Products.php
+++ src/pages/Products.php
@@ -3,7 +3,76 @@ require("../components/header.php");
 
 ?>
 
-<h1>Productds Page</h1>
+<h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">WOMEN  WEAR</h3>
+<div class="container d-flex justify-content-space-between flex-wrap gap-4">
+    <div class="card" style="width: 18rem;">
+        <img src="https://phenav.com/wp-content/uploads/2024/08/PHV_1062_Crepe-Sky-blu-5-1.jpg" class="card-img-top" alt="a long frok image">
+        <div class="card-body">
+            <h5 class="card-title">WESTERN WEAR</h5>
+            <p class="card-text">The fashion that will never let you down.</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://www.jiomart.com/images/product/original/rvgq7zemz7/sancia-women-georgette-ethnic-top-palazzo-ethnic-jacket-set-for-women-girls-ethnic-wear-for-women-indian-dress-for-women-kurta-set-with-dupatta-floral-embroidered-kurta-maroon-m-product-images-rvgq7zemz7-0-202311071449.jpg?im=Resize=(500,630)" class="card-img-top" alt="A maroon ethenic dress">
+        <div class="card-body">
+            <h5 class="card-title">ETHENIC WEAR </h5>
+            <p class="card-text">Let Your Dress Do The Walking.</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://images-static.nykaa.com/media/catalog/product/4/5/459410fn73395_1.jpg?tr=w-500" class="card-img-top" alt="JEWELLARY IMAGE">
+        <div class="card-body">
+            <h5 class="card-title">ORNAMENTS</h5>
+            <p class="card-text">Jewellery is more than adornment; it's a statement..</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://www.algenhealthcare.co/wp-content/uploads/2024/09/Top-15-Cosmetic-Manufacturers-In-Gujarat.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">COSMETICS</h5>
+            <p class="card-text">Never ask a girl with winged eyeliner why she’s late.</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+</div>
+<h3 class="p-3 mb-2 bg-primary text-white text-center text-boald">MEN  WEAR</h3>
+<div class="container d-flex justify-content-space-between flex-wrap gap-4">
+    <div class="card" style="width: 18rem;">
+        <img src="https://assets.myntassets.com/w_412,q_60,dpr_2,fl_progressive/assets/images/5394086/2024/3/27/fac7a437-ba0a-425b-8591-1e55480312611711539929437-Hancock-Men-Black-Slim-Fit-Solid-Formal-Shirt-53171153992888-1.jpg" class="card-img-top" alt="a shirt image">
+        <div class="card-body">
+            <h5 class="card-title">SHIRTS</h5>
+            <p class="card-text">The fashion that will never let you down.</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://assets.ajio.com/medias/sys_master/root/20231214/wOQq/657ad716afa4cf41f5cff1ec/-473Wx593H-464104985-maroon-MODEL.jpg" class="card-img-top" alt="A maroon ethenic dress">
+        <div class="card-body">
+            <h5 class="card-title">T-SHIRTS </h5>
+            <p class="card-text">Let Your Dress Do The Walking.</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMhn8LbSHzkcnBhfKg8HKIs8ysR5PWdFApKQ&s" class="card-img-top" alt="JEWELLARY IMAGE">
+        <div class="card-body">
+            <h5 class="card-title">WATCHES</h5>
+            <p class="card-text">Jewellery is more than adornment; it's a statement..</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://i.pinimg.com/236x/30/15/c2/3015c2d9f8a8f2a4d34d1777c0c2942e.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">SUITS</h5>
+            <p class="card-text">Fashions fade, style is eternal.</p>
+            <a href="#" class="btn btn-primary">CLICK HERE</a>
+        </div>
+    </div>
+</div>
 <?php
 require("../components/footer.php");
 ?>
